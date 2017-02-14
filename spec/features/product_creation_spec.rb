16d03cfef21d434c5b37338582b468809ba8221c
Andrea D'Amico <andrea.damico@gmail.com>
2017-02-14 11:24:20 +0100

    product update spec

--- spec/features/product_creation_spec.rb
+++ spec/features/product_creation_spec.rb
@@ -7,4 +7,12 @@ feature 'A user wanting to contribute to the knowledge' do
     click_on 'Créer ce Produit'
     expect(page).to have_content('Produit abarfoo créé avec succès')
   end
+
+  scenario 'modifies an existing product' do
+    product = create(:product)
+    visit edit_product_path(product)
+    fill_in 'product_name', with: 'new name'
+    click_on 'Modifier ce Produit'
+    expect(page).to have_content('Produit new name mis à jour avec succès')
+  end
 end
