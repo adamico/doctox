a478db0aed4034cee53980018af9820dfa352c33
Andrea D'Amico <andrea.damico@gmail.com>
2017-02-15 20:21:36 +0100

    commentaries

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,5 +1,13 @@
+# This model will be abstracted to DomainModel
+# depending on use case this could be either Mono or Polymorphic
+# e.g. for DocTox the DomainModel is a substance/product/drug
+
 class Product < ApplicationRecord
+  # tagging contexts will be dynamically defined during runtime choosing them
+  # from a custom list saved in the site settings
+  # e.g. for DocTox we have synonyms, symptoms, exposition contexts
   acts_as_taggable
+
   scope :search_import, -> { includes(:tags) }
 
   searchkick language: 'french'
@@ -11,10 +19,6 @@ class Product < ApplicationRecord
 
   def self.recent(number = RECENT_NUMBER)
     order(:updated_at).limit(number)
-  end
-
-  def name_and_class
-    "#{name} (#{model_name.human})"
   end
 
   def search_data
